c7885960a4cb14a47fb7f0e7cd2da2c7a79dac05
Iliya Shaina <iliia.shaina@neuro-city.ru>
2019-06-07 21:10:14 +0300

    login button and password autofocus

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import {createStackNavigator, createAppContainer} from 'react-navigation';
 import NotesScreen from "./screens/NotesScreen";
 import FoldersScreen from "./screens/FoldersScreen";
 import {PersistGate} from 'redux-persist/integration/react';
+import { Ionicons } from '@expo/vector-icons';
+import { Font } from 'expo';
 
 import {store, persistor} from './redux/store';
 import {Provider} from "react-redux";
 import EditNoteScreen from "./screens/EditNoteScreen";
 import LogInScreen from "./screens/LogInScreen";
-
 const RootStack = createStackNavigator(
     {
         LogIn: LogInScreen,
@@ -24,6 +25,14 @@ const RootStack = createStackNavigator(
 const AppContainer = createAppContainer(RootStack);
 
 class App extends React.Component {
+    async componentDidMount() {
+        await Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font,
+        });
+    }
+
     render() {
         return (
             <Provider store={store}>
--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import {Container, Content, Form, Item, Input, Text} from 'native-base';
+import {Container, Content, Form, Item, Input, Text, Button} from 'native-base';
 import validator from "email-validator";
 import {logIn} from "../redux/actions/authActions";
 import {connect} from "react-redux";
+import {Ionicons} from "@expo/vector-icons";
+import {Font} from 'expo';
 
 const styles = {
     Container: {
@@ -34,15 +36,26 @@ class LogInScreen extends Component {
         super(props);
         this.state = {
             email: '',
-            password: ''
-        }
+            password: '',
+            fontLoaded: false
+        };
+        this.inputs = {};
     }
 
-    componentDidMount() {
+
+
+    async componentDidMount() {
         if (this.props.isLogined) {
             this.props.navigation.push('Folders');
         }
+        await Font.loadAsync({
+            'Roboto': require('native-base/Fonts/Roboto.ttf'),
+            'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+            ...Ionicons.font,
+        });
+        this.setState({fontLoaded: true})
     }
+
 
     componentDidUpdate(prevProps, prevState, snapshot) {
         if (this.state.email.toLowerCase() === userData.email && this.state.password === userData.password) {
@@ -65,6 +78,10 @@ class LogInScreen extends Component {
 
     isCorrectEmail = () => this.state.email.toLowerCase() === userData.email;
 
+    focusTheField = (id) => {
+        console.log(this.inputs);
+        this.inputs[id]._root.focus();
+    };
 
     renderInputs = () => {
         return (
@@ -76,12 +93,16 @@ class LogInScreen extends Component {
                     <Input
                         style={styles.Input}
                         placeholder="email"
+                        autoCapitalize={"none"}
                         value={this.state.email}
+                        returnKeyType={ 'next' }
                         onChangeText={this.changeEmailHandler}
+                        onSubmitEditing={() => { this.focusTheField('field2'); }}
                     />
                 </Item>
                 <Item last>
                     <Input
+                        ref={input => { this.inputs['field2'] = input }}
                         secureTextEntry={true} style={styles.Input}
                         value={this.state.password}
                         placeholder="Password"
@@ -95,14 +116,23 @@ class LogInScreen extends Component {
     render() {
         return (
             <Container style={styles.Container}>
-                <Content>
-                    <Text style={styles.Text}>email: admin@yandex.ru, Password: admin </Text>
-                    {this.renderInputs()}
-                </Content>
+                {this.state.fontLoaded ? (
+                    <Content>
+                        <Text style={styles.Text}>email: admin@yandex.ru, Password: admin </Text>
+                        {this.renderInputs()}
+                        <Button full primary style={{paddingBottom: 4}} title={"Login"}>
+                            <Text>Login</Text>
+                        </Button>
+                    </Content>
+                ) : (
+                    <Text>loading...</Text>
+                )}
+
             </Container>
         );
     }
 }
+
 
 const mapStateToProps = state => {
     return {
@@ -111,9 +141,9 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => {
-        return {
-            logIn: () => dispatch(logIn())
-        }
-    };
+    return {
+        logIn: () => dispatch(logIn())
+    }
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(LogInScreen);
